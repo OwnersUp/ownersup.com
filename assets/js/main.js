0a6462fdf5ef2b1fec5c31f3d2c71fd2fc47cffd
Daniel <daniel@defaria.me>
2017-08-09 23:30:52 -0400

    Added new home page, pricing, blank template as Jekyll templates. Added responsive design.

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function ($) {
+    
+    AOS.init({
+        duration: 300,
+        easing: 'ease-out-quad',
+        delay: 100,
+        once:true	
+    });
+
+    $(".swiper-container").each(function () {
+
+        var sliderId = "#" + $(this).attr("id");
+
+        var quoteSwiper = new Swiper(sliderId, {
+            speed:1000,
+            parallax: true,
+            pagination: '.swiper-pagination',
+            paginationClickable: true,
+            nextButton: '.swiper-button-next',
+            prevButton: '.swiper-button-prev'
+        });
+
+    });
+
+    $(document).foundation();
+
+});
